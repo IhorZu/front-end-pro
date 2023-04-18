a46c27c03bfa997f5518edaa2934b5a3c66b9bee
Ihor Zuikov <ihor.zuikov@gmail.com>
2023-04-18 23:11:35 +0300

    6th homework

--- js/homework_6.js
+++ js/homework_6.js
@@ -1,56 +1,63 @@
 'use strict';
 
+let ageAlertText,
+    cityAlertText,
+    sportAlertText;
+
 // get birth year
 const birthYear = +prompt('Enter your year of birth, number only');
 
 if (birthYear) {
     const date = new Date();
     const age = date.getFullYear() - birthYear;
-    alert(`You are ${age} years old or will be this year`);
+    ageAlertText = `You are ${age} years old or will be this year.`
+
+    // get city
+    const myCity = prompt('What city do you live in?');
+
+    if (myCity) {
+        switch (myCity) {
+            case 'Kyiv':
+                cityAlertText = `You live in the capital of Ukraine.`;
+                break;
+            case 'London':
+                cityAlertText = `You live in the capital of Great Britain.`;
+                break;
+            case 'Washington':
+                cityAlertText = `You live in the capital of USA.`;
+                break;
+            default:
+                cityAlertText = `You live in ${myCity}.`;
+                break;
+        }
+
+        // get favorite sport
+        const favoriteSport = prompt('What is your favorite sport?');
+
+        if (favoriteSport) {
+            switch (favoriteSport) {
+                case 'football':
+                    sportAlertText = `Cool! Do you wanna be like Lionel Messi?`;
+                    break;
+                case 'basketball':
+                    sportAlertText = `Cool! Do you wanna be like Michael Jordan?`;
+                    break;
+                case 'boxing':
+                    sportAlertText = `Cool! Do you wanna be like Mike Tyson?`;
+                    break;
+                default:
+                    sportAlertText = `Cool! ${favoriteSport} is nice.`;
+                    break;
+            }
+
+            // display result
+            alert(`${ageAlertText}\n${cityAlertText}\n${sportAlertText}`);
+        } else {
+            alert('You didn\'t enter favorite sport');
+        }
+    } else {
+        alert('You didn\'t enter a city');
+    }
 } else {
     alert('You didn\'t enter a year');
-}
-
-// get city
-const myCity = prompt('What city do you live in?');
-
-if (myCity) {
-    switch (myCity) {
-        case 'Kyiv':
-            alert(`You live in the capital of Ukraine`);
-            break;
-        case 'London':
-            alert(`You live in the capital of Great Britain`);
-            break;
-        case 'Washington':
-            alert(`You live in the capital of USA`);
-            break;
-        default:
-            alert(`You live in ${myCity}`);
-            break;
-    }
-} else {
-    alert('You didn\'t enter a city');
-}
-
-// get favorite sport
-const favoriteSport = prompt('What is your favorite sport?');
-
-if (favoriteSport) {
-    switch (favoriteSport) {
-        case 'football':
-            alert(`Cool! Do you wanna be like Lionel Messi?`);
-            break;
-        case 'basketball':
-            alert(`Cool! Do you wanna be like Michael Jordan?`);
-            break;
-        case 'boxing':
-            alert(`Cool! Do you wanna be like Mike Tyson?`);
-            break;
-        default:
-            alert(`Cool! ${favoriteSport} is nice`);
-            break;
-    }
-} else {
-    alert('You didn\'t enter favorite sport');
 }
