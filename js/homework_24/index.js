e59adefcff22310c1bca987e484cb392c3731325
Ihor Zuikov <ihor.zuikov@gmail.com>
2023-06-01 00:02:35 +0300

    homework 24

--- js/homework_24/index.js
+++ js/homework_24/index.js
@@ -1,19 +1,16 @@
 'use strict';
 
-const root = document.querySelector('#root');
-
-for (let i = 1; i <= 10; i++) {
-    const div = document.createElement('div');
-    div.classList.add('row');
-
-    for (let j = 1; j <= 10; j++) {
-        const randomNumber = Math.ceil(Math.random() * 100);
-        const span = document.createElement('span');
-
-        span.classList.add('col');
-        span.append(`${randomNumber}`);
-        div.append(span);
+const httpsCheck = (element) => {
+    const href = element.getAttribute('href');
+    if (!href.includes('https')) {
+        httpsSet(element, href);
     }
-
-    root.append(div);
 }
+
+const httpsSet = (element, href) => {
+    element.setAttribute('href', `https://${href}`);
+}
+
+const buttons = document.querySelectorAll('.button');
+
+buttons.forEach(httpsCheck);
